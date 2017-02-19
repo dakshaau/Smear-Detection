86dc8ae0d41072a67df89c3ad8bff207bce4091c
Daksh Gupta <dakshkumargupta@gmail.com>
2017-02-19 01:21:00 -0600

    A step closer to detecting the smears

--- hw1.py
+++ hw1.py
@@ -43,21 +43,28 @@ if __name__ == '__main__':
 		tot = len(files)
 		print(dir)
 		c_masks = []
+		count = 0
+		avg = []
+		avg_grad = []
 		for i,f in enumerate(files):
 			img = cv2.imread(dir+'/'+f,0)
 			h,w = img.shape
 			ratio = w/float(h)
 			img = cv2.resize(img, (int(ratio*720), 720))
-			h,w = img.shape
-			new_h = int(h/3)
-			new_w = int(w/3)
-			imgs = [img[:new_h,:new_w], img[new_h:2*new_h,:new_w], img[2*new_h:,:new_w],
-					img[:new_h,new_w:2*new_w], img[new_h:2*new_h,new_w:2*new_w], img[2*new_h:,new_w:2*new_w],
-					img[:new_h,2*new_w:], img[new_h:2*new_h,2*new_w:], img[2*new_h:,2*new_w:],
-					]
-			
-			for x,im in enumerate(imgs):
-				imgs[x] = process_image(im)
+			# h,w = img.shape
+			# new_h = int(h/3)
+			# new_w = int(w/3)
+			if i == 0:
+				avg = np.zeros(img.shape,dtype=np.float64)
+				avg_grad = np.zeros(img.shape,dtype=np.float64)
+			avg_img = np.mean(img)
+			if avg_img > 15:
+				count += 1
+				avg += np.float64(img)
+				grad = cv2.Laplacian(img, cv2.CV_64F)
+				avg_grad += grad
+			# for x,im in enumerate(imgs):
+				# imgs[x] = process_image(im)
 			# img = cv2.equalizeHist(img)
 			# img -= 70
 			# for k in range(img.shape[0]):
@@ -67,32 +74,32 @@ if __name__ == '__main__':
 			flag = False
 			
 
-			if len(masks) == 0:
-				masks = [np.ones(x.shape,dtype=x.dtype) for x in imgs]
-			else:
+			# if len(masks) == 0:
+			# 	masks = [np.ones(x.shape,dtype=x.dtype) for x in imgs]
+			# else:
 				
-				c_masks = sub(imgs,prev)
-				# if i%2 != 0:
-				# 	invert = lambda k: 255*np.ones(k.shape,dtype=k.dtype) - k
-				# 	c_masks = [invert(x) for x in c_masks]
-				temp = c_masks[:]
-				for x,c in enumerate(c_masks):
-					c_mask = ''
-					if i%2 == 0:
-						r, c_mask = cv2.threshold(c_masks[x], 127,255, cv2.THRESH_BINARY_INV)
-						flag = True
-					else:
-						r, c_mask = cv2.threshold(c_masks[x], 127,255, cv2.THRESH_BINARY)
-						flag = False
-					c_masks[x] = c_mask
-				masks = [cv2.bitwise_and(l,m) for l,m in zip(c_masks,masks)]
-				while True:
-					key = cv2.waitKey(10)
-					cv2.imshow('disp',c_masks[8])
-					cv2.imshow('image',imgs[8])
-					if key == 27:
-						break
-			prev = imgs[:]
+			# 	c_masks = sub(imgs,prev)
+			# 	# if i%2 != 0:
+			# 	# 	invert = lambda k: 255*np.ones(k.shape,dtype=k.dtype) - k
+			# 	# 	c_masks = [invert(x) for x in c_masks]
+			# 	temp = c_masks[:]
+			# 	for x,c in enumerate(c_masks):
+			# 		c_mask = ''
+			# 		if i%2 == 0:
+			# 			r, c_mask = cv2.threshold(c_masks[x], 127,255, cv2.THRESH_BINARY_INV)
+			# 			flag = True
+			# 		else:
+			# 			r, c_mask = cv2.threshold(c_masks[x], 127,255, cv2.THRESH_BINARY)
+			# 			flag = False
+			# 		c_masks[x] = c_mask
+			# 	masks = [cv2.bitwise_and(l,m) for l,m in zip(c_masks,masks)]
+			# 	while True:
+			# 		key = cv2.waitKey(10)
+			# 		cv2.imshow('disp',c_masks[8])
+			# 		cv2.imshow('image',imgs[8])
+			# 		if key == 27:
+			# 			break
+			# prev = imgs[:]
 
 			# if mask.__class__ is list:
 			# 	mask = np.zeros(img.shape,dtype=img.dtype)
@@ -113,8 +120,18 @@ if __name__ == '__main__':
 				# print(mask[:10,:10])
 
 			pro = ((i+1.)/tot)*100
-			sys.stdout.write("\rCompleted: %.2f%% %d %d" % (pro,i,flag))
+			sys.stdout.write("\rCompleted: %.2f%% %d %d" % (pro,i,count))
 			# sys.stdout.flush()
 			# prev = img
-		cv2.imwrite(d+'_mask.jpg',mask)
+		avg /= count
+		avg_grad /= count
+		avg = np.uint8(avg)
+		avg_grad = np.uint8(avg_grad)
+		while True:
+			key = cv2.waitKey(10)
+			if key == 27:
+				break
+			cv2.imshow('Average Image', avg)
+			cv2.imshow('Average Grdient', avg_grad)
+		# cv2.imwrite(d+'_mask.jpg',mask)
 		print('\n')
