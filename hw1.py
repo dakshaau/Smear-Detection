3fe4b5d8dfc23976aedd69eadc7e45820f934d6d
Daksh Gupta <dakshkumargupta@gmail.com>
2017-02-19 19:39:07 -0600

    Histogram stetching

--- hw1.py
+++ hw1.py
@@ -28,10 +28,14 @@ def process_image(imag):
     r, gr = cv2.threshold(gr,127,255,cv2.THRESH_BINARY)
     return np.uint8(gr)
 
+# def ransac(n, d, k, t):
+
+# 	pass
+
 if __name__ == '__main__':
 	dirs, x = getList('sample_drive')
 	# print(dirs)
-	dirs = ['cam_3']
+	# dirs = ['cam_3']
 	for d in dirs:
 		x, files = getList('sample_drive/'+d)
 		# print(len(files))
@@ -46,92 +50,67 @@ if __name__ == '__main__':
 		count = 0
 		avg = []
 		avg_grad = []
-		for i,f in enumerate(files):
-			img = cv2.imread(dir+'/'+f,0)
-			h,w = img.shape
-			ratio = w/float(h)
-			img = cv2.resize(img, (int(ratio*720), 720))
-			# h,w = img.shape
-			# new_h = int(h/3)
-			# new_w = int(w/3)
-			if i == 0:
-				avg = np.zeros(img.shape,dtype=np.float64)
-				avg_grad = np.zeros(img.shape,dtype=np.float64)
-			avg_img = np.mean(img)
-			if avg_img > 15:
-				count += 1
-				avg += np.float64(img)
-				grad = cv2.Laplacian(img, cv2.CV_64F)
-				avg_grad += grad
-			# for x,im in enumerate(imgs):
-				# imgs[x] = process_image(im)
-			# img = cv2.equalizeHist(img)
-			# img -= 70
-			# for k in range(img.shape[0]):
-			# 	for l in range(img.shape[1]):
-			# 		if img[k,l] < 0 :
-			# 			img[k,l] = 0
-			flag = False
-			
-
-			# if len(masks) == 0:
-			# 	masks = [np.ones(x.shape,dtype=x.dtype) for x in imgs]
-			# else:
+		if not os.path.exists(d+'_avg_img.jpg'):
+			for i,f in enumerate(files):
+				img = cv2.imread(dir+'/'+f)
+				h,w = img.shape[:2]
+				ratio = w/float(h)
+				img = cv2.resize(img, (int(ratio*720), 720))
+				# h,w = img.shape
+				# new_h = int(h/3)
+				# new_w = int(w/3)
+				if i == 0:
+					avg = np.zeros(img.shape,dtype=np.float64)
+					avg_grad = np.zeros((img.shape[:2]),dtype=np.float64)
+				avg_img = np.mean(img)
+				if avg_img > 15:
+					count += 1
+					avg += np.float64(img)
+					img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+					grad = cv2.Laplacian(img, cv2.CV_64F)
+					avg_grad += np.float64(grad)
+				# for x,im in enumerate(imgs):
+					# imgs[x] = process_image(im)
+				# img = cv2.equalizeHist(img)
+				# img -= 70
+				# for k in range(img.shape[0]):
+				# 	for l in range(img.shape[1]):
+				# 		if img[k,l] < 0 :
+				# 			img[k,l] = 0
+					flag = False
 				
-			# 	c_masks = sub(imgs,prev)
-			# 	# if i%2 != 0:
-			# 	# 	invert = lambda k: 255*np.ones(k.shape,dtype=k.dtype) - k
-			# 	# 	c_masks = [invert(x) for x in c_masks]
-			# 	temp = c_masks[:]
-			# 	for x,c in enumerate(c_masks):
-			# 		c_mask = ''
-			# 		if i%2 == 0:
-			# 			r, c_mask = cv2.threshold(c_masks[x], 127,255, cv2.THRESH_BINARY_INV)
-			# 			flag = True
-			# 		else:
-			# 			r, c_mask = cv2.threshold(c_masks[x], 127,255, cv2.THRESH_BINARY)
-			# 			flag = False
-			# 		c_masks[x] = c_mask
-			# 	masks = [cv2.bitwise_and(l,m) for l,m in zip(c_masks,masks)]
-			# 	while True:
-			# 		key = cv2.waitKey(10)
-			# 		cv2.imshow('disp',c_masks[8])
-			# 		cv2.imshow('image',imgs[8])
-			# 		if key == 27:
-			# 			break
-			# prev = imgs[:]
 
-			# if mask.__class__ is list:
-			# 	mask = np.zeros(img.shape,dtype=img.dtype)
-			# 	# ret, mask = cv2.threshold(mask, 50,255,cv2.THRESH_BINARY)
-			# else:
-			# 	c_mask = prev - img
-			# 	ret,c_mask = cv2.threshold(c_mask, 100,255, cv2.THRESH_BINARY)
-			# 	# mask = cv2.bitwise_or(mask,c_mask)
-			# 	# mask+=img
-			# 	# t = mask*255
-			# 	while True:
-			# 		key = cv2.waitKey(10)
-			# 		cv2.imshow('disp',img)
-			# 		if key == 27:
-			# 			break
-				# if np.array_equal(mask, flag):
-					# break
-				# print(mask[:10,:10])
+				pro = ((i+1.)/tot)*100
+				sys.stdout.write("\rCompleted: %.2f%% %d %d" % (pro,i,count))
+				# sys.stdout.flush()
+				# prev = img
+			avg /= count
+			avg_grad /= count
+			avg = np.uint8(avg)
+			avg_grad = np.uint8(avg_grad)
+			# while True:
+			# 	key = cv2.waitKey(10)
+			# 	if key == 27:
+			# 		break
+			# 	cv2.imshow('Average Image', avg)
+			# 	cv2.imshow('Average Grdient', avg_grad)
+			cv2.imwrite(d+'_avg_img.jpg',avg)
+			cv2.imwrite(d+'_avg_grad.jpg',avg_grad)
+			print('\n')
+		else:
+			img = cv2.imread(d+'_avg_img - Copy.jpg',0)
+			grad = cv2.imread(d+'_avg_grad.jpg',0)
 
-			pro = ((i+1.)/tot)*100
-			sys.stdout.write("\rCompleted: %.2f%% %d %d" % (pro,i,count))
-			# sys.stdout.flush()
-			# prev = img
-		avg /= count
-		avg_grad /= count
-		avg = np.uint8(avg)
-		avg_grad = np.uint8(avg_grad)
-		while True:
-			key = cv2.waitKey(10)
-			if key == 27:
-				break
-			cv2.imshow('Average Image', avg)
-			cv2.imshow('Average Grdient', avg_grad)
-		# cv2.imwrite(d+'_mask.jpg',mask)
-		print('\n')
+			imgi = np.int32(img)
+			r,c = np.where(imgi < 90)
+			imgi[r,c] -= 40
+			r,c = np.where(imgi >= 90)
+			imgi[r,c] += 40
+			imgi = np.uint8(imgi)
+
+			while True:
+				key = cv2.waitKey(10)
+				if key == 27:
+					break
+				cv2.imshow('Dilated Gradient',imgi)
+				# cv2.imshow('Gradient',im)
